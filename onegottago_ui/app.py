0537c06e9ce04611b44ce8b5d96a512cec46c5d5
ariesunique <ariesunique@gmail.com>
2022-12-30 18:59:24 -0500

    Update UI to read from API

--- onegottago_ui/app.py
+++ onegottago_ui/app.py
@@ -1,12 +1,25 @@
 from flask import Flask, render_template
+import random
+import requests
+
 
 app = Flask(__name__)
+app.config.from_pyfile("settings.py")
 
 @app.route('/')
-def hello():
-    message = "Hello World!"
+def index():
 
-    return render_template('index.html', message=message)
+    api_url = app.config.get("API_URL")
+    ids = app.config.get("DATASTORE_IDS").split(",")
+
+    id = random.choice(ids)
+
+    response = requests.get(f"{api_url}/cards/{id}")
+    json_response = response.json()
+    category = json_response["category"]
+    options = json_response["options"]
+
+    return render_template('index.html', category=category, options=options)
 
 
 if __name__ == '__main__':
---
+++ onegottago_ui/settings.py
@@ -0,0 +1,4 @@
+import os
+
+API_URL = os.environ.get("API_URL")
+DATASTORE_IDS = os.environ.get("DATASTORE_IDS")
